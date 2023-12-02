9cc01bdc2676b758ab97118516b8fcffce8f0b02
Craig Andrews <craigeandrews@gmail.com>
2023-12-02 14:57:57 +0000

    more tidying

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -14,20 +14,22 @@ const MAX_BLUE: u32 = 14;
 fn part1() {
     let matcher: regex::Regex = Regex::new(r"(\d+) (red|green|blue)").unwrap();
     let mut game: u32 = 0;
-    let result = io::stdin()
-        .lock()
-        .lines()
-        .map(|line| -> u32 {
-            game += 1;
-            if matcher.captures_iter(&line.unwrap()).any(|capture| match &capture[2] {
+    let lines = io::stdin().lock().lines();
+    let result = lines.fold(0, |acc, line| -> u32 {
+        game += 1;
+        let is_impossible = matcher.captures_iter(&line.unwrap()).any(|capture| match &capture[2] {
                         "red" => MAX_RED,
                         "green" => MAX_GREEN,
                         "blue" => MAX_BLUE,
                         _ => 0,
                     } <= capture[1].parse().unwrap()
-                ) { 0 } else { game }
-        })
-        .sum::<u32>();
+                );
+        if is_impossible {
+            acc
+        } else {
+            acc + game
+        }
+    });
 
     println!("{}", result);
 }
@@ -35,24 +37,21 @@ fn part1() {
 #[allow(dead_code)]
 fn part2() {
     let matcher: regex::Regex = Regex::new(r"(\d+) (red|green|blue)").unwrap();
-    let result = io::stdin()
-        .lock()
-        .lines()
-        .map(|line| {
-            matcher
-                .captures_iter(&line.unwrap())
-                .fold((0, 0, 0), |acc, capture| {
-                    let value: u32 = capture[1].parse().unwrap();
-                    match &capture[2] {
-                        "red" => (cmp::max(acc.0, value), acc.1, acc.2),
-                        "green" => (acc.0, cmp::max(acc.1, value), acc.2),
-                        "blue" => (acc.0, acc.1, cmp::max(acc.2, value)),
-                        _ => acc,
-                    }
-                })
-        })
-        .map(|game| game.0 * game.1 * game.2)
-        .sum::<u32>();
+    let lines = io::stdin().lock().lines();
+    let result = lines.fold(0, |acc, line| {
+        let game = matcher
+            .captures_iter(&line.unwrap())
+            .fold((0, 0, 0), |acc, capture| {
+                let value: u32 = capture[1].parse().unwrap();
+                match &capture[2] {
+                    "red" => (cmp::max(acc.0, value), acc.1, acc.2),
+                    "green" => (acc.0, cmp::max(acc.1, value), acc.2),
+                    "blue" => (acc.0, acc.1, cmp::max(acc.2, value)),
+                    _ => acc,
+                }
+            });
+        acc + game.0 * game.1 * game.2
+    });
 
     println!("{:?}", result);
 }
