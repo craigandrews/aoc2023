c188de991b282fa89b721cb4a4a09d0f0cf2746f
Craig Andrews <craigeandrews@gmail.com>
2023-12-02 14:46:37 +0000

    more tidying

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -13,38 +13,33 @@ const MAX_BLUE: u32 = 14;
 #[allow(dead_code)]
 fn part1() {
     let matcher: regex::Regex = Regex::new(r"(\d+) (red|green|blue)").unwrap();
-    let stdin = io::stdin();
     let mut game: u32 = 0;
-    println!(
-        "{}",
-        stdin
-            .lock()
-            .lines()
-            .map(|line| -> u32 {
-                game += 1;
-                if matcher.captures_iter(&line.unwrap()).any(|capture| match &capture[2] {
+    let result = io::stdin()
+        .lock()
+        .lines()
+        .map(|line| -> u32 {
+            game += 1;
+            if matcher.captures_iter(&line.unwrap()).any(|capture| match &capture[2] {
                         "red" => MAX_RED,
                         "green" => MAX_GREEN,
                         "blue" => MAX_BLUE,
                         _ => 0,
                     } <= capture[1].parse().unwrap()
                 ) { 0 } else { game }
-            })
-            .sum::<u32>()
-    );
+        })
+        .sum::<u32>();
+
+    println!("{}", result);
 }
 
 #[allow(dead_code)]
 fn part2() {
     let matcher: regex::Regex = Regex::new(r"(\d+) (red|green|blue)").unwrap();
-    let stdin = io::stdin();
-
-    println!(
-        "{:?}",
-        stdin
-            .lock()
-            .lines()
-            .map(|line| matcher
+    let result = io::stdin()
+        .lock()
+        .lines()
+        .map(|line| {
+            matcher
                 .captures_iter(&line.unwrap())
                 .fold((0, 0, 0), |acc, capture| {
                     let value: u32 = capture[1].parse().unwrap();
@@ -54,8 +49,10 @@ fn part2() {
                         "blue" => (acc.0, acc.1, cmp::max(acc.2, value)),
                         _ => acc,
                     }
-                }))
-            .map(|game| game.0 * game.1 * game.2)
-            .sum::<u32>()
-    );
+                })
+        })
+        .map(|game| game.0 * game.1 * game.2)
+        .sum::<u32>();
+
+    println!("{:?}", result);
 }
