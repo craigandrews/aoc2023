c4a4925f060320aaa7cb88a46b198caa92999922
Craig Andrews <craigeandrews@gmail.com>
2023-12-13 08:15:47 +0100

    part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,5 +1,4 @@
-use itertools::Itertools;
-use std::io;
+use std::{collections::HashMap, io};
 
 type Counts = Vec<usize>;
 
@@ -17,46 +16,100 @@ fn read_input() -> Vec<Record> {
         .collect()
 }
 
-fn is_valid(rec: &Record) -> bool {
-    rec.0
-        .iter()
-        .group_by(|c| *c)
-        .into_iter()
-        .filter_map(|(k, g)| match k {
-            '#' => Some(g.count()),
-            _ => None,
-        })
-        .eq(rec.1.iter().copied())
-}
-
 fn count_matches(rec: &Record) -> usize {
-    let pos = rec.0.iter().position(|c| *c == '?');
-    match pos {
-        Some(pos) => {
-            let mut as_empty = rec.0.clone();
-            as_empty[pos] = '.';
-
-            let mut as_spring = rec.0.clone();
-            as_spring[pos] = '#';
-
-            count_matches(&(as_empty, rec.1.clone())) + count_matches(&(as_spring, rec.1.clone()))
+    fn inner_(
+        springs: &[char],
+        counts: &[usize],
+        cache: &mut HashMap<(usize, usize), usize>,
+    ) -> usize {
+        if counts.is_empty() {
+            // if any damaged, then we've run out of groups
+            // otherwise all remaining are ok and we found a possible match
+            return if springs.contains(&'#') { 0 } else { 1 };
         }
-        None => {
-            if is_valid(rec) {
-                1
-            } else {
-                0
-            }
+
+        if springs.len() < counts.iter().sum::<usize>() + counts.len() {
+            // not enough space left for the remaining groups plus
+            // their required boundary
+            return 0;
         }
+
+        // cache key is the count and spring lengths
+        let key = (counts.len(), springs.len());
+
+        if cache.contains_key(&key) {
+            // if we've seen this before just return it
+            return cache[&key];
+        }
+
+        let mut matches = 0;
+
+        if springs[0] != '#' {
+            // assume next one is not damaged so skip it
+            matches += inner_(&springs[1..].to_vec(), counts, cache);
+        }
+
+        let next = counts[0];
+        if !springs[..next].contains(&'.') && springs[next] != '#' {
+            // consider the next block of springs.
+            // none of them are ok, and the spring following the block is
+            // not damaged, therefore the next group fits into the
+            // next block of springs.
+            // seems to work?
+            matches += inner_(&springs[next + 1..], &counts[1..], cache);
+        }
+
+        // cache and move on, we're done here
+        cache.insert(key, matches);
+        matches
     }
+
+    // add a . so it's easier to detect the end of the slice
+    // without causing a panic by checking beyond bounds
+    let mut springs = rec.0.clone();
+    springs.push('.');
+
+    // cache with a key of remaining spring length and remaining groups len
+    // or (counts.len(), springs.len())
+    let mut cache: HashMap<(usize, usize), usize> = HashMap::new();
+
+    inner_(&springs, &rec.1, &mut cache)
 }
 
 fn main() {
     let input = read_input();
     part1(&input);
+    part2(&input);
 }
 
 fn part1(input: &Vec<Record>) {
+    let total: usize = input.iter().map(|rec| count_matches(rec)).sum();
+    println!("{}", total);
+}
+
+fn part2(input: &Vec<Record>) {
+    let input: Vec<Record> = input
+        .iter()
+        .map(|rec| {
+            let chars: Vec<char> = rec
+                .0
+                .iter()
+                .chain([&'?'])
+                .cycle()
+                .take((rec.0.len() + 1) * 5 - 1)
+                .map(|c| *c)
+                .collect();
+            let counts: Vec<usize> = rec
+                .1
+                .iter()
+                .cycle()
+                .take(rec.1.len() * 5)
+                .map(|v| *v)
+                .collect();
+
+            (chars, counts)
+        })
+        .collect();
     let total: usize = input.iter().map(|rec| count_matches(rec)).sum();
     println!("{}", total);
 }
